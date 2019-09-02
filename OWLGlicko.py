42f96f5e84eac24847cd7abc056b69e416ccc6ce
Steven <shepsteven42@gmail.com>
2019-09-01 20:02:56 -0700

    Initial Commit

---
+++ OWLGlicko.py
@@ -0,0 +1,491 @@
+import argparse
+import json
+import urllib.request as url
+import os
+import csv
+from glicko2 import glicko2
+from statistics import mean
+import random
+from outcomes import calc_prob_win_ft4, get_wins_ft4, calc_prob_win_rs, get_wins_rs
+import numpy
+
+def period_range(x):
+    x = int(x)
+    if x <= 0:
+        raise argparse.ArgumentTypeError("Period cannot be less or equal to 0")
+    return x
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--update", action="store_true",
+	help="Update match data from api.overwatchleague.com")
+parser.add_argument("--random", action="store_true",
+	help="Randomize game order before rating. Use this to mitigate recency bias.")
+parser.add_argument("--score", action="store_true",
+	help="Score the system parameters' prediction accuracy throughout the season.")
+parser.add_argument("-c", "--crosstable", action="store_true",
+	help="Generate expected outcomes in each maptype. Outputs to Crosstable.csv.")
+parser.add_argument("-t", "--timeline", action="store_true",
+	help="Generate rating history, taken every 10 maps played. Outputs to Timeline.csv.")
+parser.add_argument("-p", "--periods", type=period_range, default=25, metavar='PERIOD',
+	help="Split the season into P rating periods.")
+parser.add_argument("-r", "--rating", type=float, default=1500, metavar='RATING',
+	help="Set the starting rating for each player. Default=1500")
+parser.add_argument("-d", "--deviation", type=float, default=400, metavar='DEV',
+	help="Set the starting rating deviation for each player. Default=350")
+parser.add_argument("-v", "--volatility", type=float, default=0.08, metavar='VOL',
+	help="Set the starting volatility for each player. Default=0.03")
+
+args = parser.parse_args()
+
+team_names = [
+	"ATL","BOS","CDH","DAL","FLA",
+	"GLA","GZC","HOU","HZS","LDN",
+	"NYE","PAR","PHI","SEO","SFS",
+	"SHD","TOR","VAL","VAN","WAS"
+]
+maptypes = ["control","assault","hybrid", "escort", "all"]
+randCount = 50
+
+
+# Pulls new match data from api.overwatchleague.com and outputs to MapList.csv in the working directory
+def update_mapfile():
+	print(" Update triggered. Updating MapFile...")
+	contents = url.urlopen("https://api.overwatchleague.com/schedule").read()
+
+	schedule = json.loads(contents)
+	stages = schedule["data"]["stages"]
+
+	strings = []
+	
+	CSVString = "GameID, MatchID, Visitor, Vabbr, Home, Habbr, V Score, H Score, Draws, GameNo, VPoints, HPoints, BuildID, InstanceID, MapGUID, State, Start Date, StageID, Match Type, V1, V2, V3, V4, V5, V6, H1, H2, H3, H4, H5, H6 \n"
+	strings.append(CSVString)
+
+	for s in stages:
+		StageName = s["name"]
+		print("  Updating matches in", StageName, "...")
+		count = 0
+		if StageName != "All-Stars":
+			for m in s["matches"]:
+				if count % 10 == 9:
+					print("    ", count+1, "matches parsed")
+				count = count + 1
+				if m["state"] == "CONCLUDED":
+					MatchID = m["id"]
+					Visitor = m["competitors"][0]["name"]
+					Vabbr   = m["competitors"][0]["abbreviatedName"]
+					Home	= m["competitors"][1]["name"]
+					Habbr   = m["competitors"][1]["abbreviatedName"]
+					VID		= m["competitors"][0]["id"]
+					HID		= m["competitors"][1]["id"]
+					VScore 	= m["wins"][0]
+					HScore	= m["wins"][1]
+					Draws	= m["ties"][0]
+					SDate	= m["startDateTS"]
+					StageID = m["bracket"]["id"]
+					MatchType = m["bracket"]["type"]
+					State 	= m["state"]
+					contents = url.urlopen("https://api.overwatchleague.com/match/" + str(MatchID)).read()
+					MatchJson = json.loads(contents)
+					for g in m["games"]:
+						GameID	= g["id"]
+						GameNo	= g["number"]
+						VPoints = g["points"][0]
+						HPoints = g["points"][1]
+						try:
+							BuildID = g["attributes"]["build"]
+						except:
+							BuildID = 60433
+						MapGUID = g["attributes"]["mapGuid"]
+						Instance = g["attributes"]["instanceID"]
+						VPlayers = []
+						HPlayers = []
+						for p in MatchJson["games"][GameNo - 1]["players"]:
+							if VID == p["team"]["id"]:
+								VPlayers.append(p["player"]["id"])
+							else:
+								HPlayers.append(p["player"]["id"])
+						VPString = ""
+						HPString = ""
+						for i in VPlayers:
+							VPString = VPString + str(i) + " , "
+						for i in HPlayers:
+							HPString = HPString + str(i) + " , "
+						CSVString = (	str(GameID) + " , " +
+										str(MatchID) + " , " + 
+										str(Visitor) + " , " + 
+										str(Vabbr) + " , " + 
+										str(Home) + " , " + 
+										str(Habbr) + " , " + 
+										str(VScore) + " , " + 
+										str(HScore) + " , " + 
+										str(Draws) + " , " + 
+										str(GameNo) + " , " + 
+										str(VPoints) + " , " + 
+										str(HPoints) + " , " + 
+										str(BuildID) + " , " + 
+										str(Instance) + " , " + 
+										str(MapGUID) + " , " + 
+										str(State) + " , " + 
+										str(SDate) + " , " + 
+										str(StageID) + " , " + 
+										str(MatchType) + " , " + 
+										VPString + HPString
+										+ str(0) + "\n"
+									)
+						strings.append(CSVString)
+				else:
+					try:
+						MatchID = m["id"]
+						Visitor = m["competitors"][0]["name"]
+						Vabbr   = m["competitors"][0]["abbreviatedName"]
+						Home	= m["competitors"][1]["name"]
+						Habbr   = m["competitors"][1]["abbreviatedName"]
+						VID		= m["competitors"][0]["id"]
+						HID		= m["competitors"][1]["id"]
+						VScore 	= m["wins"][0]
+						HScore	= m["wins"][1]
+						Draws	= m["ties"][0]
+						SDate	= m["startDateTS"]
+						StageID = m["bracket"]["id"]
+						MatchType = m["bracket"]["type"]
+						State 	= m["state"]
+
+						CSVString = (	str("") + " , " +
+											str(MatchID) + " , " + 
+											str(Visitor) + " , " + 
+											str(Vabbr) + " , " + 
+											str(Home) + " , " + 
+											str(Habbr) + " , " + 
+											str(VScore) + " , " + 
+											str(HScore) + " , " + 
+											str(Draws) + " , " + 
+											" , , , , , , " +
+											str(State) + " , " + 
+											str(SDate) + " , " + 
+											str(StageID) + " , " + 
+											str(MatchType) + " , " +
+											"\n"
+										)
+						strings.append(CSVString)
+					except:
+						pass
+	MapCSV = open("MapList.csv", "w")
+	for s in strings:
+		MapCSV.write(s)
+	MapCSV.close()
+	print("MapFile update complete.")
+	print()
+
+def get_matches(mapfile="MapList.csv"):
+	contents = url.urlopen("https://api.overwatchleague.com/maps").read()
+	maps = json.loads(contents)
+	mapLookup = {}
+
+	result = []
+	for m in maps:
+		GUID = m["guid"]
+		TYPE = m["type"]
+		mapLookup[GUID] = TYPE
+
+	with open(mapfile, mode='r') as infile:
+		infile.readline()
+		reader = csv.reader(infile)
+		X = True
+		for i in reader:
+			if i[15].strip() == "CONCLUDED":
+				outcome = 0.5
+				V = i[3].strip()
+				H = i[5].strip()
+				maptype = mapLookup[i[14].strip()]
+				if i[10] > i[11]:
+					outcome = 1.
+				elif i[11] > i[10]:
+					outcome = 0.
+
+				result.append([V, H, maptype, outcome])
+	return result
+
+def crosstable_predictions(teams, maptypes, outfile="Crosstable.csv"):
+	draw_odds = {
+		"control":	0.00,
+		"assault":	0.073089701,
+		"hybrid":	0.02302631579,
+		"escort":	0.00,
+		"all":		0.01966955153
+	}
+	with open(outfile, "w") as out:
+		result = []
+		csvstring = "team 1, team 2, control, assault, hybrid, escort, all, rs, ft4\n"
+		out.write(csvstring)
+		rsmatches = get_wins_rs()
+		ft4matches = get_wins_ft4()
+		for t1 in teams:
+			for t2 in teams:
+				if t1 < t2:
+					csvstring = t1 + "," + t2 + ","
+					probs = {}
+					for mt in maptypes:
+						result.append(t1)
+						result.append(t2)
+						r1 = teams[t1][mt].rating
+						rd1 = teams[t1][mt].rd
+						r2 = teams[t2][mt].rating
+						rd2 = teams[t2][mt].rd
+						m = teams[t1][mt].expected_outcome(r2, rd2)
+						probs[mt] = {}
+						probs[mt][1] 	= m / (1+draw_odds[mt])
+						probs[mt][0] 	= draw_odds[mt] / (1+draw_odds[mt])
+						probs[mt][-1] 	= (1-m) / (1+draw_odds[mt])
+						csvstring += str(m) + ","
+					p1 = calc_prob_win_rs(probs, rsmatches)
+					p2 = calc_prob_win_ft4(probs, ft4matches)
+					# print("  ", t1, t2, p2)
+					out.write(csvstring + str(p1) + "," + str(p2) + "\n")
+
+def rating_pool(games, rating=args.rating, rd = args.deviation, vol = args.volatility, period=args.periods, randomizer = False, tau=0.8):
+	teams = {}
+	for t in team_names:
+		teams.update({t:{}})
+		for m in maptypes:
+			teams[t].update({m:glicko2.Player(rating=rating, rd=rd, vol=vol, tau=tau)})
+
+	elo_over_time = {}
+	for t in team_names:
+		elo_over_time.update({t:{}})
+		for m in maptypes:
+			elo_over_time[t].update({m:[]})
+
+	count = {"control":0, "hybrid":0, "assault":0, "escort":0, "all":0}
+	score = {"control":0, "hybrid":0, "assault":0, "escort":0, "all":0}
+	score2 = {"control":0, "hybrid":0, "assault":0, "escort":0, "all":0}
+
+	if randomizer:
+		random.shuffle(games)
+
+	period_size = round(len(games)/period)
+	#print("Period size:", period_size)
+	num_periods = period
+	#print("Period count:", num_periods)
+	ranges = []
+	for i in range(int(num_periods)):
+		r = [i * period_size, (i+1)*period_size]
+		if r[1] > len(games):
+			r[1] = len(games)
+			ranges.append(r)
+			break
+		ranges.append(r)
+	rcount = 1
+	for r in ranges:
+		#print(rcount)
+		gameset = games[r[0]:r[1]]
+		to_update = {}
+		for t in teams:
+			to_update[t] = {}
+		for t in teams:
+			for m in maptypes:
+				to_update[t][m] = {"ratings":[], "rds":[], "outcomes":[]}
+		for g in gameset:
+			t1 = g[0]
+			t2 = g[1]
+			mt = g[2]
+			oc = g[3]
+			# if t1 == "HZS" or t2 == "HZS":
+			# 	print(g)
+
+			to_update[t1][mt]["ratings"] 	+= [teams[t2][mt].rating]
+			to_update[t1][mt]["rds"] 		+= [teams[t2][mt].rd]
+			to_update[t1][mt]["outcomes"] 	+= [oc]
+			to_update[t2][mt]["ratings"] 	+= [teams[t1][mt].rating]
+			to_update[t2][mt]["rds"] 		+= [teams[t1][mt].rd]
+			to_update[t2][mt]["outcomes"] 	+= [1-oc]
+
+			expected = teams[t1][mt].expected_outcome(teams[t2][mt].rating, teams[t2][mt].rd)
+			s = (round(expected) * oc + round(1-expected)*(1-oc))
+			score[mt] += s * rcount
+			count[mt] += rcount
+			#print(round(expected), s, oc)
+
+			mt = "all"
+			to_update[t1][mt]["ratings"] 	+= [teams[t2][mt].rating]
+			to_update[t1][mt]["rds"] 		+= [teams[t2][mt].rd]
+			to_update[t1][mt]["outcomes"] 	+= [oc]
+			to_update[t2][mt]["ratings"] 	+= [teams[t1][mt].rating]
+			to_update[t2][mt]["rds"] 		+= [teams[t1][mt].rd]
+			to_update[t2][mt]["outcomes"] 	+= [1-oc]
+
+			expected = teams[t1][mt].expected_outcome(teams[t2][mt].rating, teams[t2][mt].rd)
+			s = (round(expected) * oc + round(1-expected)*(1-oc))
+			score[mt] += s * rcount
+			count[mt] += rcount
+		for t in teams:
+			for m in maptypes:
+				if len(to_update[t][m]["ratings"]) > 0:
+					teams[t][m].update_player(to_update[t][m]["ratings"],to_update[t][m]["rds"],to_update[t][m]["outcomes"])
+				else:
+					teams[t][m].did_not_compete()
+		#rcount += 1/num_periods
+
+	if args.timeline:
+		for team in teams:
+			for m in teams[team]:
+				elo_over_time[team][m].append(teams[team][m].rating)
+	
+	
+	for g in games:
+		t1 = g[0]
+		t2 = g[1]
+		mt = g[2]
+		oc = g[3]
+		expected = teams[t1][mt].expected_outcome(teams[t2][mt].rating, teams[t2][mt].rd)
+		s = expected * oc + (1-expected)*(1-oc)
+		score2[mt] += s
+
+		mt = "all"
+		expected = teams[t1][mt].expected_outcome(teams[t2][mt].rating, teams[t2][mt].rd)
+		s = expected * oc + (1-expected)*(1-oc)
+		score2[mt] += s
+	
+	for m in score:
+		score[m] /= count[m]
+	for m in score2:
+		score2[m] /= count[m]
+		#print(m, score2[m])
+	# for m in maptypes:
+	# 	for t in teams:
+	# 		print(t, m, teams[t][m].rating, teams[t][m].rd, teams[t][m].vol)
+	return teams, elo_over_time, score
+
+def main():
+	
+	#print(args)
+
+	if args.update:
+		update_mapfile()
+	elif not(os.path.exists("MapList.csv")):
+		print(" MapList.csv does not exist. Updating...")
+		update_mapfile()
+
+	print(" Initializing rating system with parameters:")
+	print(" ", args.rating, args.deviation, args.volatility)
+
+	games = get_matches("MapList.csv")
+	if args.random:
+		print(" Rating " + str(len(games)) + " games " + str(randCount) + " times...")
+		teams_list = []
+		elo_over_time_list = []
+		score_list = []
+		for i in range(randCount):
+			t, eot, s = rating_pool(games, randomizer=args.random, period=args.periods)
+			teams_list.append(t)
+			elo_over_time_list.append(eot)
+			score_list.append(s)
+		
+		teams = teams_list.pop()
+		elo_over_time = elo_over_time_list.pop()
+		score = score_list.pop()
+		for i in range(randCount-1):
+			other = teams_list.pop()
+			for t in teams:
+				for m in maptypes:
+					teams[t][m].rating = ((i+1)*teams[t][m].rating + other[t][m].rating) / (i+2)
+					teams[t][m].rd = ((i+1)*teams[t][m].rd + other[t][m].rd) / (i+2)
+					teams[t][m].vol = ((i+1)*teams[t][m].vol + other[t][m].vol) / (i+2)
+			other = elo_over_time_list.pop()
+			for t in teams:
+				for m in maptypes:
+					for k in range(len(elo_over_time[t][m])):
+						elo_over_time[t][m][k] = ((i+1)*elo_over_time[t][m][k] + other[t][m][k]) / (i+2)
+			other = score_list.pop()
+			for m in maptypes:
+				score[m] = ((i+1)*score[m] + other[m]) / (i+2)
+
+	else:
+		print(" Rating " + str(len(games)) + " games...")
+		teams, elo_over_time, score = rating_pool(games, randomizer=args.random, period=args.periods)
+
+	print(" scores:")
+	for m in maptypes:
+		print("",round(score[m], 3))
+	with open("Ratings.csv", "w") as outfile:
+		csvstring = "Team" + ","
+		for m in maptypes:
+			csvstring += m + ","
+		csvstring += "\n"
+		outfile.write(csvstring)
+		for i in teams:
+			csvstring = i + ","
+			for m in maptypes:
+				csvstring += str(teams[i][m].rating) + ","
+			csvstring +="\n"
+			outfile.write(csvstring)
+		print(" Ratings output to file 'Ratings.csv'")
+
+	if args.timeline:
+		with open("Timeline.csv", "w") as outfile:
+			for m in maptypes:
+				csvstring = m + '\n'
+				outfile.write(csvstring)
+				for i in elo_over_time:
+					csvstring = i + ","
+					for j in elo_over_time[i][m]:
+						csvstring += str(j) + ","
+					csvstring+= "\n"
+					outfile.write(csvstring)
+				csvstring = '\n'
+				outfile.write(csvstring)
+			print(" Timeline output to file 'Timeline.csv'")
+
+	if args.crosstable:
+		crosstable_predictions(teams, maptypes)
+		print(" Crosstable output to file 'Crosstable.csv'")
+
+def testparams():
+	games = get_matches()
+	results = []
+	maxes = {}
+
+
+	for m in maptypes:
+		maxes[m] = [0,0,0,0,0,0,0,0,0]
+	for rd in numpy.arange(400, 401, 25):
+		for vol in numpy.arange(0.08,0.081, 0.01):
+			for period in [20,40,60,80,100,200,400,800,1000,1200]:
+				for tau in numpy.arange(0.2,0.8, 0.1):
+					teams, elo_over_time, score = rating_pool(games, rating=args.rating, rd=rd, vol=(vol), period =(period), tau=tau, randomizer = False)
+					# for t in teams:
+					# 	print(t, teams[t]["control"].rating)
+					r = []
+					r.append(rd)
+					r.append(vol)
+					r.append(period)
+					r.append(tau)
+					for m in maptypes:
+						r.append(score[m])
+					results.append(r)
+					i = 4
+					for m in maptypes:
+						if maxes[m][i] < score[m]:
+							maxes[m] = r
+						i+=1
+					for i in range(len(r)):
+						r[i] = round(r[i], 5)
+					print(r)
+	print()
+	for m in maptypes:
+		print(m[0], maxes[m])
+
+	with open("TestParams.csv", "w") as outfile:
+		csvstring = "RD, VOL, PER,"
+		for m in maptypes:
+			csvstring+=m + ","
+		outfile.write(csvstring + "\n")
+		for i in results:
+			csvstring = ""
+			for k in i:
+				csvstring+= str(k) + ","
+			outfile.write(csvstring+ "\n")
+
+
+main()
+
+#testparams()
---
+++ bracket.py
@@ -0,0 +1,367 @@
+import csv
+from statistics import mean, mode
+import random
+import argparse
+import os
+
+def alpha_range(x):
+    x = float(x)
+    if x < 0 or x > 1:
+        raise argparse.ArgumentTypeError("Alpha out of range [0,1]")
+    return x
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--alpha", type=alpha_range, default=0, metavar='A',
+	help="Default 0. Factor to bias match outcome probabilities from Crosstable values towards 50/50. 0<=a<=1")
+parser.add_argument("-s", "--seed", action="store_true",
+	help="Add existing match outcomes before running bracket simulations.")
+
+args = parser.parse_args()
+
+def get_seeds(b = []):
+	q = [b]
+	result = []
+	while len(q) > 0:
+		bi = q.pop(0)
+		if len(bi) == 18:
+			result += [bi]
+			continue
+		q.append(bi + [-1])
+		q.append(bi + [1])
+	return result
+
+def get_probs(file="Crosstable.csv"):
+	if not os.path.exists(file):
+		raise NameError("'Crosstable.csv' not found. Run 'OWLGLicko.py -c' first.")
+	result = {}
+	with open(file, mode='r') as infile:
+		infile.readline()
+		reader = csv.reader(infile)
+		X = True
+		for i in reader:
+			V = i[0].strip()
+			H = i[1].strip()
+			p = float(i[8].strip())
+			if not(V in result.keys()):
+				result[V] = {}
+			result[V][H] = p
+	return result
+
+probs = get_probs()
+
+def simulate_seeded(seed = []):
+	seeds = get_seeds(seed)	
+	print("  All child seeds computed. Running", "2^"+str(18 - len(seed)), "simulations...")	
+	winners = {}
+	ranklist = {
+		"VAN": {},
+		"NYE": {},
+		"SFS": {},
+		"HZS": {},
+		"GLA": {},
+		"ATL": {},
+		"LDN": {},
+		"SEO": {},
+		"GZC": {},
+		"PHI": {},
+		"SHD": {},
+		"CDH": {}
+	}
+	b = Bracket(seeds[0])
+	count = 0
+	for s in seeds:
+		# count += 1
+		# if count % 1000 == 0:
+		# 	print("Bracket #", count)
+		b.set_seed(s)
+		r, w = b.results()
+		pbracket = b.eval()
+		if not(w in winners):
+			winners[w] = 0
+		winners[w] += pbracket
+		for t in ranklist:
+			if not(r[t] in ranklist[t].keys()):
+				ranklist[t][r[t]] = 0
+			ranklist[t][r[t]] += pbracket
+
+	total = 0
+	for t in winners:
+		total += winners[t]
+	sorted_winners = sorted(winners.items(), key=lambda kv: kv[1])
+	print()
+	print("  Team | Chance of 1st")
+	print("  -----|--------------")
+	sorted_winners.reverse()
+	for i in sorted_winners:
+		print("  ", i[0], "|", round(i[1] / total, 4))
+	print()
+
+	average_ranks = {}
+
+	with open("Placings.csv", "w") as outfile:
+		csvstring = "Team,"
+		for i in [1,2,3,4,5,7,9,11]:
+			csvstring += str(i) + ","
+		csvstring += "Avg Place"
+		outfile.write(csvstring+"\n")
+		for t in ranklist:
+			csvstring = t + ","
+			e = 0
+			d = sum(ranklist[t].values())
+			for i in [1,2,3,4,5,7,9,11]:
+				if i not in ranklist[t].keys():
+					ranklist[t][i] = 0
+				e += i * ranklist[t][i]
+				csvstring += str(ranklist[t][i] / d) +","
+			csvstring += str(e/d)
+			outfile.write(csvstring+"\n")
+			average_ranks[t] = e/d
+
+	sorted_ranks = sorted(average_ranks.items(), key=lambda kv: kv[1])
+	print("  Team | Avg Rank")
+	print("  -----|---------")
+	for i in sorted_ranks:
+		print("  ", i[0], "|", round(i[1], 4))
+	print()
+
+	print("Writing results to 'Placings.csv'...")
+
+class Match:
+	def __init__(self, outcome, team1=None, team2=None, child1 = (None, 0), child2 = (None, 0)):
+		self.team1 = team1
+		self.team2 = team2
+		self.outcome = outcome
+		self.child1 = child1
+		self.child2 = child2
+		self.update()
+	
+	def set_team(self, i, t):
+		if i == 1:
+			self.team1 = t
+		if i == 2:
+			self.team2 = t
+
+	def set_child(self, i, c):
+		if i == 1:
+			self.child1 = c
+		if i == 2:
+			self.child2 = c
+
+	def set_teams(self, t1, t2):
+		self.set_team(1, t1)
+		self.set_team(2, t2)
+
+	def set_children(self, c1, c2):
+		self.set_child(1, c1)
+		self.set_child(2, c2)
+
+	#returns probability team1 will win against team2
+	def prob(self):
+		t1 = self.team1
+		t2 = self.team2
+		if t1 == None:
+			t1 = self.get_team_child(1)
+		if t2 == None:
+			t2 = self.get_team_child(2)
+		if t1 == None or t2 == None:
+			return None
+		if t1 < t2:
+			p = probs[t1][t2]
+		else:
+			p = 1 - probs[t2][t1]
+
+		if self.outcome == 1:
+			return (1-args.alpha)*p + args.alpha*0.5
+		if self.outcome == -1:
+			return (1-args.alpha)*(1-p) + args.alpha*0.5
+		else:
+			return None
+
+	def update(self):
+		# if self.child1[0] != None:
+		# 	self.child1[0].update()
+		# if self.child2[0] != None:
+		# 	self.child2[0].update()
+
+		t1 = self.get_team_child(1)
+		t2 = self.get_team_child(2)
+		if t1 != None:
+			self.team1 = t1
+		if t2 != None:
+			self.team2 = t2
+
+	def get_team_child(self, i):
+		if i == 1:
+			c = self.child1
+		if i == 2:
+			c = self.child2
+		if c[0] == None:
+			return None
+		if c[1] == 0:
+			return c[0].winner()
+		if c[1] == 1:
+			return c[0].loser()
+		else:
+			return None
+
+	def teams(self):
+		return [self.team1, self.team2]
+	def winner(self):
+		if self.outcome == 1:
+			return self.team1
+		elif self.outcome == -1:
+			return self.team2
+		else:
+			return None
+	def loser(self):
+		if self.outcome == -1:
+			return self.team1
+		elif self.outcome == 1:
+			return self.team2
+		else:
+			return None
+	def print(self):
+		print(self.teams())
+		print(self.prob())
+		print(self.winner())
+		print()
+
+class Bracket:
+	def __init__(self, seed):
+		self.matches = []
+		for i in range(18):
+			self.matches.append(Match(seed[i]))
+		self.update()
+
+	def update(self):
+		rank = {
+			"VAN":1,
+			"NYE":2,
+			"SFS":3,
+			"HZS":4,
+			"GLA":5,
+			"ATL":6,
+			"LDN":7,
+			"SEO":8,
+			"GZC":9,
+			"PHI":10,
+			"SHD":11,
+			"CDH":12
+		}
+
+		#PLAYINS
+		self.matches[0].set_teams("GZC", "CDH")
+		self.matches[1].set_teams("PHI", "SHD")
+		
+		self.matches[2].set_team(1, "LDN")
+		self.matches[3].set_team(1, "SEO")
+		if rank[self.matches[0].winner()] > rank[self.matches[1].winner()]:
+			self.matches[2].set_child(2, (self.matches[0], 0))
+			self.matches[3].set_child(2, (self.matches[1], 0))
+		else:
+			self.matches[2].set_child(2, (self.matches[1], 0))
+			self.matches[3].set_child(2, (self.matches[0], 0))
+		self.matches[2].update()
+		self.matches[3].update()
+		
+		# PLAYOFFS RD1
+		self.matches[4].set_team(1, "VAN")
+		self.matches[5].set_teams("HZS", "GLA")
+		self.matches[6].set_team(1, "NYE")   
+		self.matches[7].set_teams("SFS", "ATL")
+		if rank[self.matches[2].winner()] > rank[self.matches[3].winner()]:
+			self.matches[4].set_child(2, (self.matches[2], 0))
+			self.matches[6].set_child(2, (self.matches[3], 0))
+		else:
+			self.matches[4].set_child(2, (self.matches[3], 0))
+			self.matches[6].set_child(2, (self.matches[2], 0))
+
+		self.matches[8].set_children((self.matches[4],1), (self.matches[5],1))
+		self.matches[9].set_children((self.matches[6],1), (self.matches[7],1))
+		self.matches[10].set_children((self.matches[4],0), (self.matches[5],0))
+		self.matches[11].set_children((self.matches[6],0), (self.matches[7],0)) 
+		self.matches[12].set_children((self.matches[11],1), (self.matches[8],0))
+		self.matches[13].set_children((self.matches[10],1), (self.matches[9],0)) 
+		self.matches[14].set_children((self.matches[12],0), (self.matches[13],0))  
+		self.matches[15].set_children((self.matches[10],0), (self.matches[11],0))
+		self.matches[16].set_children((self.matches[15],1), (self.matches[14],0))  
+		self.matches[17].set_children((self.matches[15],0), (self.matches[16],0))
+
+		for i in range(18):
+			self.matches[i].update()
+
+	def set_seed(self, seed):
+		for i in range(18):
+			self.matches[i].outcome = seed[i]
+		self.update()
+	
+	def eval(self):
+		r = 1
+		for m in self.matches:
+			r *= m.prob()
+		return r
+
+	def print(self):
+		for i in self.matches:
+			print(i.team1, i.team2, i.outcome)
+
+	def results(self):
+		r = {}
+		w = self.matches[17].winner()
+		r[self.matches[17].winner()] = 1
+		r[self.matches[17].loser()]  = 2
+		r[self.matches[16].loser()]  = 3
+		r[self.matches[14].loser()]  = 4
+		r[self.matches[12].loser()]  = 5
+		r[self.matches[13].loser()]  = 5
+		r[self.matches[8].loser()]   = 7
+		r[self.matches[9].loser()]   = 7
+		r[self.matches[2].loser()]   = 9
+		r[self.matches[3].loser()]   = 9
+		r[self.matches[0].loser()]   = 11
+		r[self.matches[1].loser()]   = 11
+		return r, w
+
+def get_inputs():
+	print("\nFollow the prompts to seed the bracket with match outcomes.")
+	b = Bracket([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+	s = []
+	for i in range(18):
+		print(b.matches[i].teams())
+		instring = input(" What is the outcome for this match?\n 1 for team1, -1 for team2, 0 to exit ")
+		instring = int(instring)
+		while not(instring in [1, -1, 0]):
+			print(" Invalid input")
+			instring = input(" What is the outcome for this match?\n 1 for team1, -1 for team2, 0 to exit ")
+			instring = int(instring)
+
+		if instring == 0:
+			b.update()
+			break
+		if instring in [1, -1]:
+			s.append(instring)
+			b.matches[i].outcome = instring
+			b.update()
+	return s
+
+
+def main():
+	s = []
+	if args.seed:
+		user_sat = False
+		while(not(user_sat)):
+			s = get_inputs()
+			print(s)
+			instring = input(" Are you satisfied with this seed? Y/N ")
+			while instring not in ["Y", "N", "y", "n"]:
+				print(" Invalid input")
+				print(s)
+				instring = input(" Are you satisfied with this bracket seed? Y/N ")
+				instring = str(instring)
+			if instring in ["Y", "y"]:
+				user_sat = True
+		print()
+	print("Running simulation with initial bracket seed:", s)
+	simulate_seeded(s)
+
+main()
